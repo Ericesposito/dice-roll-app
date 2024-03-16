442b8ee750ccb2dbeac4b92c345f2683a0f4db89
Ericesposito <eespo93@optonline.net>
2024-03-16 15:07:41 -0400

    Learning how Decorations work, and building Widget trees, moving onto modularizing and creating classes soon

--- dice_roll_app/lib/main.dart
+++ dice_roll_app/lib/main.dart
@@ -1,7 +1,31 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text("Hello World!"),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 39, 3, 101),
+                Color.fromARGB(255, 112, 77, 172),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello World!",
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
