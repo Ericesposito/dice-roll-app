317e50e592028e33efb66de774b2b4e410b5117c
Ericesposito <eespo93@optonline.net>
2024-03-17 11:26:54 -0400

    understanding differences between const and final, including logic for understanding compile-time constants and how it differs from runtime-evaluated values

---
+++ dice_roll_app/lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:dice_roll_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 39, 3, 101),
+            Color.fromARGB(255, 112, 77, 172),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
--- dice_roll_app/lib/main.dart
+++ dice_roll_app/lib/main.dart
@@ -1,30 +1,12 @@
 import "package:flutter/material.dart";
 
+import 'package:dice_roll_app/gradient_container.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 39, 3, 101),
-                Color.fromARGB(255, 112, 77, 172),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World!",
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
