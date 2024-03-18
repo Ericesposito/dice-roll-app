e7618c08d4c7a690956ceff8578a854c956daa92
Ericesposito <eespo93@optonline.net>
2024-03-18 19:38:08 -0400

    deleted unnecessary comments

--- dice_roll_app/lib/dice_roller.dart
+++ dice_roll_app/lib/dice_roller.dart
@@ -51,36 +51,3 @@ class _DiceRollerState extends State<DiceRoller> {
     );
   }
 }
-
-
-  // var activeDiceImage;
-
-  // void rollDice() {
-  //   activeDiceImage = 'assets/images/dice-4.png';
-  //   print('Changing image...');
-  // }
-
-  // List build(context) {
-  //   return [
-  //     Image.asset(
-  //       activeDiceImage,
-  //       width: 200,
-  //     ),
-  //     const SizedBox(
-  //       height: 20,
-  //     ),
-  //     TextButton(
-  //       onPressed: rollDice,
-  //       style: TextButton.styleFrom(
-  //         // padding: const EdgeInsets.only(
-  //         //   top: 20,
-  //         // ),
-  //         foregroundColor: Colors.white,
-  //         textStyle: const TextStyle(
-  //           fontSize: 28,
-  //         ),
-  //       ),
-  //       child: const Text('Roll Dice'),
-  //     ),
-  //   ];
-  // }
